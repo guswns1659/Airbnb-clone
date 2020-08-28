532e3e77c5a64ba5170dee029ab8f6520bcc3379
DevJun <guswns1659@gmail.com>
2020-08-28 22:25:11 +0900

    feat : 스프링부트 프로젝트 셋팅
    - local DB와 연결 성공

---
+++ src/main/java/com/titanic/airbnbclone/AirbnbcloneApplication.java
@@ -0,0 +1,12 @@
+package com.titanic.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbcloneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/titanic/airbnbclone/AirbnbcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanic.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
