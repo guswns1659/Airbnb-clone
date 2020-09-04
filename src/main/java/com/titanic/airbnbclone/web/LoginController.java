0f400067388eb4184ba774e4286321ce795918ff
DevJun <guswns1659@gmail.com>
2020-09-04 23:13:40 +0900

    [#15] feat : 마이그레이션

--- src/main/java/com/titanic/airbnbclone/web/LoginController.java
+++ src/main/java/com/titanic/airbnbclone/web/LoginController.java
@@ -1,6 +1,7 @@
 package com.titanic.airbnbclone.web;
 
 import com.titanic.airbnbclone.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("github")
 public class LoginController {
 
     private final LoginService loginService;
-
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
 
     @GetMapping("/oauth/callback")
     public ResponseEntity<Void> login(@RequestParam("code") String redirectCode,
